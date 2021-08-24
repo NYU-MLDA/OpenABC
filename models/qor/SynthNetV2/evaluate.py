1d479c8505ae4f3452cf0cb6bb3c33350e98f174
Animesh Basak Chowdhury <animeshbchowdhury@gmail.com>
2021-08-24 16:02:39 -0400

    Updated SynthNetV2

--- models/qor/SynthNetV2/evaluate.py
+++ models/qor/SynthNetV2/evaluate.py
@@ -6,7 +6,7 @@ from model import *
 from utils import *
 #from NetlistClassification.netlistDataset import *
 from netlistDataset import *
-
+import argparse
 import torch.nn.functional as F
 from torch_geometric.data import DataLoader
 import numpy as np
@@ -17,6 +17,14 @@ import os.path as osp
 import pickle
 import sys
 import matplotlib.pyplot as plt
+
+datasetDict =  {
+    'set1' : ["train_data_set1.csv","test_data_set1.csv"],
+    'set2' : ["train_data_set2.csv","test_data_set2.csv"],
+    'set3' : ["train_data_mixmatch_v1.csv","test_data_mixmatch_v1.csv"]
+}
+
+DUMP_DIR = None
 
 def plotChart(x,y,xlabel,ylabel,leg_label,title):
     fig = plt.figure(figsize=(10,6))
@@ -50,104 +58,102 @@ def evaluate_plot(model, device, dataloader):
     return totalMSE,batchData
 
 
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='GNN baselines on Synthesis Task Pytorch Geometric')
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--dataset', type=str, default="set1",
+                        help='Split strategy (set1/set2/set3 default: set1)')
+    parser.add_argument('--rundir', type=str, required=True,default="",
+                        help='Output directory path to store result')
+    parser.add_argument('--datadir', type=str, required=True, default="",
+                        help='Dataset directory containing processed dataset, train test split file csvs')
+    parser.add_argument('--model', type=str, required=True, default="",
+                        help='Pre-trained model name in path <rundir> (eg. gcn-epoch30-loss-0.7734.pt)')
+    args = parser.parse_args()
 
-datasetDict =  {
-    'set1' : ["train_data_set1.csv","test_data_set1.csv"],
-    'set2' : ["train_data_set2.csv","test_data_set2.csv"],
-    'v3' : ["train_data_v3.csv","test_data_v3.csv"],
-    'mixmatchv1' : ["train_data_mixmatch_v1.csv","test_data_mixmatch_v1.csv"],
-    'mixmatchv2' : ["train_data_mixmatch_v2.csv","test_data_mixmatch_v2.csv"],
-    'L1' : ["train_data_L1.csv","test_data_L1.csv"],
-    'L2' : ["test_data_L1.csv","train_data_L1.csv"],
-    'small' : ["train_data_small.csv","test_data_small.csv"]
-}
+    datasetChoice = args.dataset
+    #RUN_DIR = args.rundir
+    MODEL_NAME = args.model
 
-datasetChoice = sys.argv[1]
-RUN_DIR = sys.argv[2]
-MODEL_NAME = sys.argv[3]
+    # Hyperparameters
+    batchSize = args.batch_size  # 64
+    nodeEmbeddingDim = 3
+    synthEncodingDim = 3
 
-batchSize = 64
-nodeEmbeddingDim = 3
-synthEncodingDim = 3
-IS_STATS_AVAILABLE = True
-ROOT_DIR = '/scratch/abc586/OPENABC_DATASET'
-DUMP_DIR = osp.join('/scratch/abc586/OpenABC-dataset/SynthV16_AND',RUN_DIR)
-MODEL_PATH = osp.join(DUMP_DIR,MODEL_NAME)
+    IS_STATS_AVAILABLE = True
+    ROOT_DIR = args.datadir  # '/scratch/abc586/OPENABC_DATASET'
+    global DUMP_DIR
+    DUMP_DIR = args.rundir
+    MODEL_PATH = osp.join(DUMP_DIR,MODEL_NAME)
 
-# Load train and test datasets
-trainDS = NetlistGraphDataset(root=ROOT_DIR,filePath=datasetDict[datasetChoice][0])
-testDS = NetlistGraphDataset(root=ROOT_DIR,filePath=datasetDict[datasetChoice][1])
+    # Load train and test datasets
+    trainDS = NetlistGraphDataset(root=ROOT_DIR,filePath=datasetDict[datasetChoice][0])
+    testDS = NetlistGraphDataset(root=ROOT_DIR,filePath=datasetDict[datasetChoice][1])
 
+    if IS_STATS_AVAILABLE:
+        with open(osp.join(ROOT_DIR, 'synthesisStatistics.pickle'), 'rb') as f:
+            numGatesAndLPStats = pickle.load(f)
+    else:
+        print("\nNo pickle file found for number of gates")
+        exit(0)
 
-if IS_STATS_AVAILABLE:
-    with open(osp.join(ROOT_DIR,'desDict.pickle'),'rb') as f:
-        numGatesAndLPStats = pickle.load(f)
-else:
-    print("\nNo pickle file found for number of gates")
-    exit(0)
+    meanVarNodesDict = computeMeanAndVarianceOfNodes(numGatesAndLPStats)
 
-meanVarNodesDict = computeMeanAndVarianceOfNodes(numGatesAndLPStats)
-
-trainDS.transform = transforms.Compose([lambda data: addNormalizedGateAndLPData(data,numGatesAndLPStats,meanVarNodesDict)])
-testDS.transform = transforms.Compose([lambda data: addNormalizedGateAndLPData(data,numGatesAndLPStats,meanVarNodesDict)])
-
-
-# Define the loss function
-criterion = torch.nn.MSELoss()
-num_classes = 1
-
-
-# Define the model
-synthFlowEncodingDim = trainDS[0].synVec.size()[0]*synthEncodingDim
-
-node_encoder = NodeEncoder(emb_dim=nodeEmbeddingDim)
-synthesis_encoder = SynthFlowEncoder(emb_dim=synthEncodingDim)
-
-model = SynthNet(node_encoder=node_encoder,synth_encoder=synthesis_encoder,n_classes=num_classes,synth_input_dim=synthFlowEncodingDim,node_input_dim=nodeEmbeddingDim)
-model.load_state_dict(torch.load(MODEL_PATH))
-device = getDevice()
-model = model.to(device)
+    trainDS.transform = transforms.Compose([lambda data: addNormalizedGateAndLPData(data,numGatesAndLPStats,meanVarNodesDict)])
+    testDS.transform = transforms.Compose([lambda data: addNormalizedGateAndLPData(data,numGatesAndLPStats,meanVarNodesDict)])
 
 
+    num_classes = 1
 
-# Initialize the dataloaders
-train_dl = DataLoader(trainDS,shuffle=True,batch_size=batchSize)
-test_dl = DataLoader(testDS,shuffle=True,batch_size=batchSize)
+    # Define the model
+    synthFlowEncodingDim = trainDS[0].synVec.size()[0]*synthEncodingDim
+    node_encoder = NodeEncoder(emb_dim=nodeEmbeddingDim)
+    synthesis_encoder = SynthFlowEncoder(emb_dim=synthEncodingDim)
+
+    model = SynthNet(node_encoder=node_encoder,synth_encoder=synthesis_encoder,n_classes=num_classes,synth_input_dim=synthFlowEncodingDim,node_input_dim=nodeEmbeddingDim)
+    model.load_state_dict(torch.load(MODEL_PATH))
+    device = getDevice()
+    model = model.to(device)
+
+    # Initialize the dataloaders
+    train_dl = DataLoader(trainDS,shuffle=True,batch_size=batchSize)
+    test_dl = DataLoader(testDS,shuffle=True,batch_size=batchSize)
+
+    # Evaluate on train data
+    trainMSE,trainBatchData = evaluate_plot(model, device, train_dl)
+    NUM_BATCHES_TRAIN = len(train_dl)
+    doScatterPlot(NUM_BATCHES_TRAIN,batchSize,trainBatchData,DUMP_DIR,"train")
 
 
+    # Evaluate on test data
+    testMSE,testBatchData = evaluate_plot(model, device, test_dl)
+    NUM_BATCHES_TEST = len(test_dl)
+    doScatterPlot(NUM_BATCHES_TEST,batchSize,testBatchData,DUMP_DIR,"test")
 
-# Evaluate on train data
-trainMSE,trainBatchData = evaluate_plot(model, device, train_dl)
-NUM_BATCHES_TRAIN = len(train_dl)
-doScatterAndTopKRanking(NUM_BATCHES_TRAIN,batchSize,trainBatchData,DUMP_DIR,"train")
+    # Dump data into pickle folder
+    with open(osp.join(DUMP_DIR,'testMSE.pkl'),'wb') as f:
+        pickle.dump(testMSE,f)
 
+    with open(osp.join(DUMP_DIR,'testBatchData.pkl'),'wb') as f:
+        pickle.dump(testBatchData,f)
 
-# Evaluate on test data
-testMSE,testBatchData = evaluate_plot(model, device, test_dl)
-NUM_BATCHES_TEST = len(test_dl)
-doScatterAndTopKRanking(NUM_BATCHES_TEST,batchSize,testBatchData,DUMP_DIR,"test")
+    with open(osp.join(DUMP_DIR,'trainBatchData.pkl'),'wb') as f:
+        pickle.dump(trainBatchData,f)
 
+    print("\nTest loss.. :"+str(testMSE)+", Batchwise test loss: "+str(testMSE/NUM_BATCHES_TEST))
+    print("\nTrain loss..:"+str(trainMSE))
 
-# Dump data into pickle folder
-with open(osp.join(DUMP_DIR,'testMSE.pkl'),'wb') as f:
-    pickle.dump(testMSE,f)
+    # Plot the charts for all epochs
+    with open(osp.join(DUMP_DIR,'valid_curve.pkl'),'rb') as f:
+        valid_curve = pickle.load(f)
 
-with open(osp.join(DUMP_DIR,'testBatchData.pkl'),'wb') as f:
-    pickle.dump(testBatchData,f)
+    with open(osp.join(DUMP_DIR,'train_loss.pkl'),'rb') as f:
+        train_loss = pickle.load(f)
 
-with open(osp.join(DUMP_DIR,'trainBatchData.pkl'),'wb') as f:
-    pickle.dump(trainBatchData,f)
+    plotChart([i+1 for i in range(len(valid_curve))],valid_curve,"# Epochs","Loss","test_acc","Validation loss")
+    plotChart([i+1 for i in range(len(train_loss))],train_loss,"# Epochs","Loss","train_loss","Training loss")
 
-print("\nTest loss.. :"+str(testMSE)+", Batchwise test loss: "+str(testMSE/NUM_BATCHES_TEST))
-print("\nTrain loss..:"+str(trainMSE))
-# Plot the charts for all epochs
-'''
-with open(osp.join(DUMP_DIR,'valid_curve.pkl'),'rb') as f:
-    valid_curve = pickle.load(f)
-
-with open(osp.join(DUMP_DIR,'train_loss.pkl'),'rb') as f:
-    train_loss = pickle.load(f)
-
-plotChart([i+1 for i in range(len(valid_curve))],valid_curve,"# Epochs","Loss","test_acc","Validation loss")
-plotChart([i+1 for i in range(len(train_loss))],train_loss,"# Epochs","Loss","train_loss","Training loss")
-'''
+if __name__ == "__main__":
+    main()
